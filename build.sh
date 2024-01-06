123d1e40932b56fe1b2fe50c91853007b4311f4b
weldeon <50535591+weldeondotwav@users.noreply.github.com>
2024-01-06 14:36:48 -0600

    update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-CGO_ENABLED=1 go build -ldflags -H=windowsgui
+clean() {
+  rm -rf dist/
+}
+
+echo "cleaning..."
+clean
+
+echo "go mod tidy"
+go mod tidy
+
+mkdir dist
+
+echo "building to dist/btw.exe"
+CGO_ENABLED=1 go build -ldflags -H=windowsgui -o dist/btw.exe
